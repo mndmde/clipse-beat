c2f192d8cf9992d5c0ff39a30df77683ae3bbdf5
mnd <tyler.j.crawford@gmail.com>
2023-09-08 03:22:25 -0400

    added js file

---
+++ script.js
@@ -0,0 +1,41 @@
+function pic() {
+    var bgm = ['./bg/1.jpeg','./bg/2.jpeg','./bg/3.jpeg'];
+
+    $('html').css({
+        'background' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ') no-repeat',
+        'background-position' : 'center',
+    });
+}
+pic();
+
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+
+  function playSoundOnClick(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`); 
+
+  if (audio) {
+    key.classList.add('playing');    
+    audio.currentTime = 0;
+    audio.play();
+  }
+}
+
+keys.forEach((element, index) => element.addEventListener('click', () => playSoundOnClick({ keyCode: keys[index].getAttribute('data-key') })));
